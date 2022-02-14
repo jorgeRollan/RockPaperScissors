760557ad96c98fd0b0820c973522bd2bd788dec8
Jorge Rollan <jorgerollandiez@gmail.com>
2022-02-14 15:05:09 +0100

    version ui interactiva de 5 partidas

--- rps.js
+++ rps.js
@@ -1,10 +1,12 @@
-function computerPlay() {
+let partidas = 5; //numero de partidas a jugar
+
+function computerPlay() {//funcion random de jugadas de computer
     let plays = ["ROCK", "PAPER", "SCISSORS"];
     let play = plays[Math.trunc(Math.random() * 3)];
-    console.log("jugada de la maquina " + play);
     return play;
 }
 
+//funcion que realiza un chequeo de la entrada del usuari,no se usa en esta version
 function checkPrompt(userPrompt) {
     switch (userPrompt.toUpperCase()) {
         case "ROCK":
@@ -59,24 +61,99 @@ function onePlay(computerPlay, userPlay) {
             }
             break;
     }
-    if (winner == "DRAW") {
-        console.log(`HA HABIDO UN EMPATE CON ${userPlay}`);
-    }
-    else console.log(`el ganador fue ${winner} pc hizo ${computerPlay} user hizo ${userPlay}`);
+    displayLastMatch(userPlay, computerPlay, winner);
 }
+const body = document.querySelector("body");
+const button1 = document.createElement("button");
+button1.textContent = "PAPER";
+body.appendChild(button1);
+const button2 = document.createElement("button");
+button2.textContent = "ROCK";
+body.appendChild(button2);
+const button3 = document.createElement("button");
+button3.textContent = "SCISSORS";
+body.appendChild(button3);
 
-function startGame() {
-    let promtInput = prompt("INTRODUZCA PAPER ROCK SCISSORS o EXIT");
-    promtInput = checkPrompt(promtInput);
+const div = document.createElement("div");
+div.style.flex = auto = "1 1 auto";
+const title = document.createElement("h1")
+title.textContent = "RESULTADOS";
+const div1 = document.createElement("div");
+const maq = document.createElement("span")
+maq.textContent = "MAQUINA    ";
+const resultadoMaq = document.createElement("span")
+resultadoMaq.textContent = 0;
+resultadoMaq.className="resultadoMaq";
+div.appendChild(title);
+div1.appendChild(maq);
+div1.appendChild(resultadoMaq);
+const div2 = document.createElement("div");
+const user = document.createElement("span")
+user.textContent = "USER     ";
+const resultadoUser = document.createElement("span")
+resultadoUser.textContent = 0;
+resultadoUser.className="resultadoUser";
+div2.appendChild(user)
+div2.appendChild(resultadoUser);
+div.appendChild(div1)
+div.appendChild(div2);
+body.appendChild(div);
 
-    if (promtInput == null) {
-        console.log("SOLO VALE ROCK PAPER SCISSORS o exit"); startGame();
+function displayLastMatch(jugadaUser, jugadaCompu, winner) {
+    let displayLast = document.querySelector("body div .lastPlay");
+    let userPlay = null;
+    let computerPlay = null;
+    let winnerText = null;
+    if (!displayLast) {
+        const divLastPlay = document.querySelector("body div");
+        const resultDiv = document.createElement("div");
+        resultDiv.className = "lastPlay";
+        divLastPlay.appendChild(resultDiv);
+        displayLast = document.querySelector("body div .lastPlay");
+        userPlay = document.createElement("p")
+        userPlay.className = "pUser";
+        computerPlay = document.createElement("p");
+        computerPlay.className = "pComputer";
+        winnerText = document.createElement("p");
+        winnerText.className = "pWinner";
+        displayLast.appendChild(userPlay);
+        displayLast.appendChild(computerPlay);
+        displayLast.appendChild(winnerText)
+    }
+    userPlay = displayLast.querySelector(".pUser");
+    computerPlay = displayLast.querySelector(".pComputer");
+    winnerText = displayLast.querySelector(".pWinner");
+    userPlay.textContent = "JUGADA ULTIMA USUARIO " + jugadaUser;
+    computerPlay.textContent = "JUGADA ULTIMA COMPUTER " + jugadaCompu;
+    if (winner == "DRAW") {
+        winnerText.textContent = "EMPATE";
     }
     else {
-        if (promtInput != "EXIT") {
-            onePlay(computerPlay(), promtInput);
+        winnerText.textContent = "EL GANADOR FUE " + winner;
+    }
+    updateResults(winner);
+
+}
+
+
+const buttons = body.querySelectorAll("button");
+buttons.forEach(element => addEvents(element));
+function addEvents(element) {
+    element.addEventListener("click", function () {
+        if (partidas > 0) {
+            onePlay(computerPlay(), element.textContent);
+            partidas = partidas-1;
         }
-        else console.log("saliendo del juego")
+    });
+}
+
+function updateResults(winner){   
+    if(winner=="USER"){
+        const user = document.querySelector(".resultadoUser");
+    user.textContent = Number(user.textContent) + 1;
+    }
+    else if (winner=="COMPUTER"){
+        const maq=document.querySelector(".resultadoMaq");
+        maq.textContent = Number(maq.textContent) + 1;
     }
 }
-startGame();
